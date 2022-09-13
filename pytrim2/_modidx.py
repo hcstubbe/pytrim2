eb43c232575d9c3fc65d84d4fb5edd27eb67b1cc
hcstubbe <hstubbe@med.lmu.de>
2022-09-13 21:48:58 +0000

    added code

--- pytrim2/_modidx.py
+++ pytrim2/_modidx.py
@@ -10,4 +10,12 @@ d = { 'settings': { 'branch': 'master',
                               'pytrim2.core.demultiplex': ('core.html#demultiplex', 'pytrim2/core.py'),
                               'pytrim2.core.findAlingments': ('core.html#findalingments', 'pytrim2/core.py'),
                               'pytrim2.core.sort_records_to_file': ('core.html#sort_records_to_file', 'pytrim2/core.py'),
-                              'pytrim2.core.trim_record': ('core.html#trim_record', 'pytrim2/core.py')}}}
+                              'pytrim2.core.trim_record': ('core.html#trim_record', 'pytrim2/core.py')},
+            'pytrim2.demultiplex': { 'pytrim2.demultiplex.align_barcodes': ('demultiplex.html#align_barcodes', 'pytrim2/demultiplex.py'),
+                                     'pytrim2.demultiplex.decide_barcode_id': ( 'demultiplex.html#decide_barcode_id',
+                                                                                'pytrim2/demultiplex.py'),
+                                     'pytrim2.demultiplex.demultiplex': ('demultiplex.html#demultiplex', 'pytrim2/demultiplex.py'),
+                                     'pytrim2.demultiplex.findAlingments': ('demultiplex.html#findalingments', 'pytrim2/demultiplex.py'),
+                                     'pytrim2.demultiplex.sort_records_to_file': ( 'demultiplex.html#sort_records_to_file',
+                                                                                   'pytrim2/demultiplex.py'),
+                                     'pytrim2.demultiplex.trim_record': ('demultiplex.html#trim_record', 'pytrim2/demultiplex.py')}}}
